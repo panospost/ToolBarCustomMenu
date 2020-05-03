33311287dff86f707206491800df1bc74a10e64d
Panagiotis Postantsidis <ctw00518@criticaltechworks.com>
2020-05-03 21:06:35 +0100

    push of the gif example of functionality

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 echo "# ToolBarCustomMenu" 
+
+![](example.gif)
--- app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.jetpackcomposetest
 
+import android.animation.Animator
+import android.animation.Animator.AnimatorListener
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.animation.PathInterpolatorCompat
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,28 +24,12 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
     }
 
-//    fun showAction(view: View) {
-//        menu_button.visibility = View.GONE
-//        close_button.visibility = View.VISIBLE
-//        customViewTest.visibility = View.VISIBLE
-//        original_parent_height = (view.parent as View).measuredHeight
-//        animateMenuOptionsHeight(original_parent_height)
-//
-//    }
-//
-//    fun hideMenu(view: View) {
-//        customViewTest.visibility = View.GONE
-//        view.visibility = View.GONE
-//        menu_button.visibility = View.VISIBLE
-//        animateMenuOptionsHeight(original_parent_height)
-//
-//    }
 
     private fun animateMenuOptionsHeight(parentheight: Int) {
-        val widthAnimator = ValueAnimator.ofInt(customViewTest.height, parentheight * 2/3)
+        val widthAnimator = ValueAnimator.ofInt(customViewTest.height, parentheight * 2 / 3)
         widthAnimator.duration = 400
-        widthAnimator.interpolator =  PathInterpolatorCompat.create(0.215f, 0.610f, 0.355f, 1.000f)
-        widthAnimator.addUpdateListener (object : ValueAnimator.AnimatorUpdateListener{
+        widthAnimator.interpolator = PathInterpolatorCompat.create(0.215f, 0.610f, 0.355f, 1.000f)
+        widthAnimator.addUpdateListener(object : ValueAnimator.AnimatorUpdateListener {
             override fun onAnimationUpdate(animation: ValueAnimator?) {
                 if (animation != null) {
                     customViewTest.layoutParams.height = animation.getAnimatedValue() as Int
@@ -59,22 +49,63 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-             R.id.open ->
-             {
-                 Log.d("icon", R.drawable.ic_menu_black_24dp.toString())
-                 Log.d("icon", item.icon.toString())
-                 if(customViewTest.visibility == View.GONE) {
-                     customViewTest.visibility = View.VISIBLE
-                     item.setIcon(R.drawable.ic_close_black_24dp)
-                 } else {
-                     customViewTest.visibility = View.GONE
-                     item.setIcon(R.drawable.ic_menu_black_24dp)
-                 }
-                 return true
-             }
+            R.id.open -> {
+                Log.d("icon", R.drawable.ic_menu_black_24dp.toString())
+                Log.d("icon", item.icon.toString())
+                if (customViewTest.visibility == View.GONE) {
+                    FadeAnimation(customViewTest, true, 200)
+                    item.setIcon(R.drawable.ic_close_black_24dp)
+                } else {
+                    FadeAnimation(customViewTest, false, 200)
+                    item.setIcon(R.drawable.ic_menu_black_24dp)
+                }
+                return true
+            }
             else ->
                 return true
         }
 
+    }
+
+    private fun FadeAnimation(
+        fadeTarget: View,
+        shoudFadeIn: Boolean,
+        duration: Long
+    ) {
+        val mAnimationSet = AnimatorSet()
+        var start = 0f
+        var end: Float
+        var visibility = View.VISIBLE
+        if (shoudFadeIn) {
+            end = 1f
+        } else {
+            end = 0f
+            start = 1f
+            visibility = View.GONE
+        }
+        val fade = ObjectAnimator.ofFloat(
+            fadeTarget,
+            View.ALPHA,
+            start,
+            end
+        )
+
+        fadeTarget.visibility = View.VISIBLE
+
+
+        fade.addListener(object : AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {}
+            override fun onAnimationEnd(animation: Animator) {
+                fadeTarget.visibility = visibility
+            }
+
+            override fun onAnimationCancel(animation: Animator) {}
+            override fun onAnimationRepeat(animation: Animator) {}
+        })
+        fade.interpolator = LinearInterpolator()
+
+        mAnimationSet.duration = duration
+        mAnimationSet.play(fade)
+        mAnimationSet.start()
     }
 }
